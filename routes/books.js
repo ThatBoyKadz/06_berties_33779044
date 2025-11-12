5bb0f9102cc3fda13781c0c0cf938035b471d1ff
ThatBoyKadz <Kadirdanny@yahoo.co.uk>
2025-11-12 16:03:09 +0000

    Added MySQL app user creation and privileges to create_db.sql

--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,26 @@
 const express = require("express")
 const router = express.Router()
 
-router.get('/search',function(req, res, next){
+router.get('/search', function(req, res, next) {
     res.render("search.ejs")
 });
 
 router.get('/search-result', function (req, res, next) {
     //searching in the database
     res.send("You searched for: " + req.query.keyword)
+});
+
+// 👇 Add this new route here
+router.get('/list', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            console.error("Database error:", err);
+            return res.redirect('/'); // redirect home if there’s an error
+        }
+        res.send(result); // send the list of books as JSON
+    });
 });
 
 // Export the router object so index.js can access it
